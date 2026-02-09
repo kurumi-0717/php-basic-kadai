16f1816205e2682af00ff76be1d89bd3bcd002f5
kurumi-0717 <yuka.inoue0329@gmail.com>
2026-02-09 18:01:33 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,19 @@
+<?php
+
+$score1 = 80;
+$score2 = 50;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 85;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+$total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+$score_average = $total_score / 10;
+
+echo $score_average;
+?>
