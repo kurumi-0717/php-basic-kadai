b4cc97ce9c2eed19e57ff7a418320a09dd6f13fd
kurumi-0717 <yuka.inoue0329@gmail.com>
2026-02-21 22:03:15 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -1,36 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
 <?php
+$array = [15, 4, 18, 23, 10];
 
-function sort_2way($array, $order = 'asc') {
-    if ($order === 'asc') {
-        sort($array);
-    } elseif ($order === 'desc') {
-        rsort($array);
+function sort_2way($array, $order) {
+    if ($order === true) {
+        echo '昇順にソートします。<br>';
+        sort($array); // 配列を昇順にソート
+    } else if ($order === false) {
+        echo '降順にソートします。<br>';
+        rsort($array); // 配列を降順にソート
     }
-    
-    foreach($array as $value) {
-        echo "$value";
-        echo "<br>";
-    }
+    return $array; // ソートした配列を返す
 }
 
-$array_data = [15, 4, 18, 23, 10];
+// 昇順
+$asc_array = sort_2way($array, true);
+foreach ($asc_array as $value) {
+    echo $value . '<br>';
+}
 
-echo "Ascending Order:<br>";
-sort_2way($array_data, 'asc');
-
-echo "<br>Descending Order:<br>";
-sort_2way($array_data, 'desc');
-
+// 降順
+$desc_array = sort_2way($array, false);
+foreach ($desc_array as $value) {
+    echo $value . '<br>';
+}
 ?>
-
-</body>
-</html>
